f4f914f0faedc48b4f943796d451d449d4f82852
DaveBsns <david-flaig@gmx.de>
2022-01-11 02:57:38 +0100

    added python code

---
+++ Backend/Backend.py
@@ -0,0 +1,26 @@
+import pafy
+
+#opening the text file which contains all the links
+file = open('list.txt', 'r')
+
+#loop through each link in the file
+for line in file:
+
+    #assign link to "url" variable
+    url = line
+
+    try:
+        #passing link to pafy
+        video = pafy.new(url)
+
+        #extracting the best available audio
+        bestaudio = video.getbestaudio()
+        print(video.title)
+
+        #donwnloading the extracted audio
+        bestaudio.download()
+    # move to next link if the video is removed in the youtube platform
+    except:
+        pass
+#close the text file
+file.close()
