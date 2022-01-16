245232ff96ef18dba7a8409b769f49cd012477b1
TaLeibl <tleibl@stud.hs-heilbronn.de>
2022-01-16 17:49:26 +0100

    show chosen docs to upload

--- Frontend/script.js
+++ Frontend/script.js
@@ -86,3 +86,14 @@ async function evaluatePython() {
     }
 }
 
+async function updateList(){
+    var input = document.getElementById('file-upload');
+    var output = document.getElementById('fileList');
+    var children = "";
+    for (var i = 0; i < input.files.length; ++i) {
+        children += '<li>' + input.files.item(i).name + '</li>';
+    }
+    output.innerHTML = '<ul>'+children+'</ul>';
+}
+
+
