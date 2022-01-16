383105ea2c60516a74a6ad88eda0f0764e8d7736
DaveBsns <david-flaig@gmx.de>
2022-01-16 18:25:13 +0100

    edit filedownload

--- Frontend/script.js
+++ Frontend/script.js
@@ -3,30 +3,30 @@ const link = document.getElementById("vorname");
 
 
 function addToOutput(s) {
-    // Wert in, welcher eingegeben wird, wird in der Python Consolenansicht wiedergegeben  
-    output.value += s + "\n";
+  // Wert in, welcher eingegeben wird, wird in der Python Consolenansicht wiedergegeben  
+  output.value += s + "\n";
 }
 
 output.value = "Initializing Python...\n";
 // init Pyodide
 async function main() {
-    let pyodide = await loadPyodide({
-        indexURL: "https://cdn.jsdelivr.net/pyodide/v0.19.0/full/"
-    });
+  let pyodide = await loadPyodide({
+    indexURL: "https://cdn.jsdelivr.net/pyodide/v0.19.0/full/"
+  });
 
-    output.value += "Ready!\n";
-    return pyodide;
+  output.value += "Ready!\n";
+  return pyodide;
 }
 let pyodideReadyPromise = main();
 
 // execute with onClick
 async function evaluatePython() {
 
-    let pyodide = await pyodideReadyPromise;
+  let pyodide = await pyodideReadyPromise;
 
-    try {
-        // let output = pyodide.runPython(link.value);
-        let output = pyodide.runPython(`
+  try {
+    // let output = pyodide.runPython(link.value);
+    let output = pyodide.runPython(`
           import js
           import json
           import os
@@ -51,8 +51,8 @@ async function evaluatePython() {
               "strasse" : js.strasseInput.value,
               "Wohnort" : js.ortInput.value,
               "telefon" : js.nummerInput.value,
-              "bewerbung auf" : js.artSelect.options.selected.value,
-              "dateien" : js.datei.value
+              "bewerbung auf" : js.artSelect.value,
+              "dateien" : js.fileUpload.value
               }
             ]  
           }
@@ -69,20 +69,38 @@ async function evaluatePython() {
 
           print(data)
           `);
-        addToOutput(output);
-        let x = pyodide.runPython("open('/data.json', 'r').read()");
-        var selectedFile = document.getElementById('datei').files[0].addEventListener('change', this, false);
-        addToOutput(x);
-        //addToOutput(y);
-        //Popup Message Success
-        alert("Übermittlung der Daten erfolgreich!");
+    addToOutput(output);
+    let x = pyodide.runPython("open('/data.json', 'r').read()");
+    var selectedFile = document.getElementById('fileUpload').files[0];
+    addToOutput(x);
+    loadFileAsText();
+    //addToOutput(y);
+    //Popup Message Success
+    alert("Übermittlung der Daten erfolgreich!");
 
 
 
-    } catch (err) {
-        addToOutput(err);
-        //Popup Message Failure
-        alert("Etwas lief schief! Bitte überprüfen Sie Ihre Eingaben, die Vollständigkit dieser und versuchen es, gegebenenfalls zu einem späteren Zeitpunkt, erneut!");
-    }
+  } catch (err) {
+    addToOutput(err);
+    //Popup Message Failure
+    alert("Etwas lief schief! Bitte überprüfen Sie Ihre Eingaben, die Vollständigkit dieser und versuchen es, gegebenenfalls zu einem späteren Zeitpunkt, erneut!");
+  }
 }
 
+
+const input = document.getElementById('fileUpload');
+const downloadLink = document.getElementById('link');
+let objectURL;
+
+input.addEventListener('change', function () {
+  if (objectURL) {
+    // revoke the old object url to avoid using more memory than needed
+    URL.revokeObjectURL(objectURL);
+  }
+
+  const file = this.files[0];
+  objectURL = URL.createObjectURL(file);
+
+  downloadLink.download = file.name; // this name is used when the user downloads the file
+  downloadLink.href = objectURL;
+});
